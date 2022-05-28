a4399e68f66f9015801d860f2d704740204d8667
ned-dev-2022 <exaucengango20@gmail.com>
2022-05-29 01:58:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOOD TECH</title>
+    <link rel="stylesheet" href="./assets/bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-6.1.1-web/css/all.css">
+</head>
+<body>
+
+   <div class="container-fluid header p-0 m-0">
+    <div class="container-fluid block p-0 m-0">
+     <div class="container">
+
+      <div class="row" id="row-header">
+        <div class="col-md-6 my-md-5 pt-5">
+          <h1>GOOD TECH<span>.</span></h1>
+          <h3>Formation<span>graduite</span>en informatique</h3>
+          <p>Suite au bilan positif des précédentes formations,nous lançons une nouvelle session de juillet 2022.</p>
+          <a href="./pages/formulaire.html" class="col-md-6 block-1 py-2">Prendre mon inscription</a>
+          <a href="#contact" class="col-md-6 block-2 mx-md-5 py-2">Nous contacter</a>
+          <a href="#black" class="col-md-6 block-1 py-2">Nos archives</a>
+        </div>
+        <img src="./assets/img/NEW-LOGO-TECH.png" alt="" class="col-md-6">
+      </div>
+
+     </div>
+    </div>
+   </div> 
+
+   <div class="container my-5">
+      <div class="row"> 
+          <div class="col-md-7">
+          <h4 style="color: #000033; font-weight: bold;">Déja 8 générations formées,notre role est de donner un métier aux jeunes</h4>
+
+          <hr>
+
+          <h5 style="color: #000033; font-weight: bold;">Comment ça marche?</h5>
+
+          <ul class="px-3">
+            <li>L'inscription est à 10.000 FCFA</li>
+            <li>La formation est gratuite:pas de frais mensuel</li>
+            <li>Pour une durée de 2 mois</li>
+            <li>Une céromonie de fin de formation avec remise de certificat de fin de la formation</li>
+          </ul>
+
+          <hr>
+
+          <h5 style="color: #000033; font-weight: bold;">Nos modules:</h5>
+
+          <ol class="px-3">
+            <li><b class="text-danger">Bureautique :</b>  Word, Excel, Powerpoint</li>
+            <li><b class="text-danger">infographie :</b> Photoshop, Illustrator</li>
+            <li><b class="text-danger">Developpement Web :</b> Html, Css, bootstrap, Php, Javascript</li>
+            <li><b class="text-danger">Developpement Mobile :</b> React Native</li>
+          </ol>
+
+        </div>
+          <img src="./assets/img/couverture (2).png" alt="" class="col-md-5">   
+     </div>
+   </div>
+   <div class="container">
+       <h5 id="black">Nos archives</h5>
+        <div class="row my-3">
+        <img src="./assets/img/D-P-117.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-130.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-167.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-73.jpg" alt="" class="col-md-3">
+        </div>
+    <div class="row">
+        <img src="./assets/img/D-P-128.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-152.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-25.jpg" alt="" class="col-md-3">
+        <img src="./assets/img/D-P-159.jpg" alt="" class="col-md-3">
+    </div>
+   </div>
+
+
+   <div class="container-fluid py-3 mt-5" style="background-color: #000033;">
+    <div class="row">
+
+        <div class="col-md-12 text-center" id="contact">
+            <a href="https://api.whatsapp.com/send?phone=242068415751" class="bg-success text-light" style="display: inline-flex; justify-content: center; align-items: center; text-decoration: none; border-radius: 50%; width: 30px; height: 30px;">
+              <i class="fab fa-whatsapp">
+              </i>
+            </a>
+
+            <a href="https://www.facebook.com/search/top?q=good%20tech" target="_blank" class="bg-primary text-light" style="display: inline-flex; justify-content: center; align-items: center; text-decoration: none; border-radius: 50%; width: 30px; height: 30px;">
+              <i class="fab fa-facebook"></i>
+            </a>
+
+            <!--<p>Ce travail présenté est l'oeuvre d'un groupe de 5 apprenants de GOOD TECH session de Mars 2022.
+                Nous vous remerçions pour votre visite sur notre site internet.</p>-->
+
+                <small class="text-light d-block mt-3" style="font-size: 11px;">copyright&copy;GOOD-TECH | T2-2022</small>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
